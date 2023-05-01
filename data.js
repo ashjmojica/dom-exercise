8358e56e337019da4b938b34f4e3e24021bf2fa9
Ashley Mojica <ashjmojica@gmail.com>
2023-05-01 18:52:33 -0500

    changes

---
+++ data.js
@@ -0,0 +1,169 @@
+const nflDraftPicks = [
+  {
+    pickNumber: 1,
+    team: "Jacksonville Jaguars",
+    player: "Trevor Lawrence",
+    position: "Quarterback",
+    college: "Clemson",
+  },
+  {
+    pickNumber: 2,
+    team: "New York Jets",
+    player: "Zach Wilson",
+    position: "Quarterback",
+    college: "BYU",
+  },
+  {
+    pickNumber: 3,
+    team: "San Francisco 49ers",
+    player: "Trey Lance",
+    position: "Quarterback",
+    college: "North Dakota State",
+  },
+  {
+    pickNumber: 4,
+    team: "Atlanta Falcons",
+    player: "Kyle Pitts",
+    position: "Tight End",
+    college: "Florida",
+  },
+];
+
+const carModels = [
+   {
+      "make": "Toyota",
+      "model": "Corolla",
+      "year": 2022,
+      "price": 20500
+   },
+   {
+      "make": "Honda",
+      "model": "Civic",
+      "year": 2022,
+      "price": 21700
+   },
+   {
+      "make": "Ford",
+      "model": "Mustang",
+      "year": 2022,
+      "price": 28200
+   },
+   {
+      "make": "Chevrolet",
+      "model": "Silverado",
+      "year": 2022,
+      "price": 32800
+   },
+   {
+      "make": "BMW",
+      "model": "3 Series",
+      "year": 2022,
+      "price": 42300
+   }
+]
+
+const nflDraftPicks = [
+  {
+    pickNumber: 1,
+    team: "Jacksonville Jaguars",
+    player: "Trevor Lawrence",
+    position: "Quarterback",
+    college: "Clemson",
+  },
+  {
+    pickNumber: 2,
+    team: "New York Jets",
+    player: "Zach Wilson",
+    position: "Quarterback",
+    college: "BYU",
+  },
+  {
+    pickNumber: 3,
+    team: "San Francisco 49ers",
+    player: "Trey Lance",
+    position: "Quarterback",
+    college: "North Dakota State",
+  },
+  {
+    pickNumber: 4,
+    team: "Atlanta Falcons",
+    player: "Kyle Pitts",
+    position: "Tight End",
+    college: "Florida",
+  },
+];
+1:51
+const carModels = [
+   {
+      "make": "Toyota",
+      "model": "Corolla",
+      "year": 2022,
+      "price": 20500
+   },
+   {
+      "make": "Honda",
+      "model": "Civic",
+      "year": 2022,
+      "price": 21700
+   },
+   {
+      "make": "Ford",
+      "model": "Mustang",
+      "year": 2022,
+      "price": 28200
+   },
+   {
+      "make": "Chevrolet",
+      "model": "Silverado",
+      "year": 2022,
+      "price": 32800
+   },
+   {
+      "make": "BMW",
+      "model": "3 Series",
+      "year": 2022,
+      "price": 42300
+   }
+]
+1:53
+const topRestaurants = [
+   {
+      "name": "Noma",
+      "location": "Copenhagen, Denmark",
+      "cuisine": "New Nordic",
+      "rating": 4.7
+   },
+   {
+      "name": "Osteria Francescana",
+      "location": "Modena, Italy",
+      "cuisine": "Italian",
+      "rating": 4.6
+   },
+   {
+      "name": "El Celler de Can Roca",
+      "location": "Girona, Spain",
+      "cuisine": "Spanish",
+      "rating": 4.5
+   },
+   {
+      "name": "Mirazur",
+      "location": "Menton, France",
+      "cuisine": "French",
+      "rating": 4.4
+   },
+   {
+      "name": "Eleven Madison Park",
+      "location": "New York City, United States",
+      "cuisine": "New American",
+      "rating": 4.3
+   }
+]
+
+
+
+
+
+
+
+
+
