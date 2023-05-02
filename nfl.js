4992d5278dabf6b455e0d111153647f68ec085eb
Ashley Mojica <ashjmojica@gmail.com>
2023-05-01 21:19:36 -0500

    changes

---
+++ nfl.js
@@ -0,0 +1,37 @@
+const nflDraftPicks = [
+  {
+    pickNumber: 1,
+    team: "Jacksonville Jaguars",
+    player: "Trevor Lawrence",
+    position: "Quarterback",
+    college: "Clemson",
+  },
+  {
+    pickNumber: 2,
+    team: "New York Jets",
+    player: "Zach Wilson",
+    position: "Quarterback",
+    college: "BYU",
+  },
+  {
+    pickNumber: 3,
+    team: "San Francisco 49ers",
+    player: "Trey Lance",
+    position: "Quarterback",
+    college: "North Dakota State",
+  },
+  {
+    pickNumber: 4,
+    team: "Atlanta Falcons",
+    player: "Kyle Pitts",
+    position: "Tight End",
+    college: "Florida",
+  },
+];
+
+const nflCards = document.querySelector(".nfl-cards");
+
+for (const nflAdditions of nfl) {
+    const nflItem = document.createElement("div");
+    const nflTitle = 
+}
